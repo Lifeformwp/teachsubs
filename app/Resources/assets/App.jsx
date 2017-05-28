fb0995b76afb4844f15bad09e7f5ca6e25796269
Serhii Kondratiuk <vielon.indie@yandex.ru>
2017-05-28 14:27:28 +0300

    diploma-6.02-removed-unnecessary-file-garbage

--- app/Resources/assets/App.jsx
+++ app/Resources/assets/App.jsx
@@ -1,9 +1,49 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            translation: '',
+            meaning: ''
+        }
+    }
+    loadNotesFromServer() {
+    $.ajax({
+        url: 'http://localhost:8000/api/'+this.state.meaning+'/eng/rus',
+        success: function (data) {
+            this.setState({translation: data[0]});
+        }.bind(this)
+    });
+    }
+
     render() {
         return (
-            <h1>Reactjs works fine, so let's start building cool frontend</h1>
+            <div>
+                <div className="notes-container">
+                    {
+                    this.state.translation !== ''
+                    ?
+                    <div>
+                        <h2 className="notes-header">Перевод: {this.state.translation['text']}</h2>
+                        <h2 className="notes-header">Язык: {this.state.translation['language']}</h2>
+                    </div>
+                    : <div></div>
+                    }
+                    <div>
+                    <input
+                       type='text'
+                       onChange={event => this.setState({meaning: event.target.value})}
+                       onKeyPress={event => {
+                           if (event.key === 'Enter') {
+                               this.loadNotesFromServer()
+                           }}
+                       }></input>
+
+                    <button onClick={() => this.loadNotesFromServer()}>Submit</button>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
